ea21a5635b811adf9e4acbb5111f217bfc551534
Tomasz Ciborski <tilosag@gmail.com>
2020-01-06 12:20:30 +0100

    refactor to per-request

--- app.js
+++ app.js
@@ -1,47 +1,48 @@
-var express = require('express'),
-	request = require('request'),
-	cheerio = require('cheerio'),
-	app = express(),
-	url = 'http://www.cpubenchmark.net/CPU_mega_page.html',
-	cpuData = [];
+var express = require('express');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
 
-request(url, function(error, response, html){
-    if(!error){
-    	
-        var $ = cheerio.load(html);
+app.get('/', function (req, res) {
+	var url = 'http://www.cpubenchmark.net/CPU_mega_page.html';
+	var cpuData = [];
 
-        $("tbody tr[id^=cpu]").each(function(x){
-			var $ref = $(this),				
-		  	$refExpandedContents = $ref.next().find("td div").contents();	
+	request(url, function (error, response, html) {
+		if (!error) {
 
-		  	cpuData.push({
-		 		name: $ref.find("td a:nth-child(2)").text() || '',
-				clockSpeed: parseInt($refExpandedContents[1].data.match(/\d+(?!.*\d)/)) || 0,
-				turboSpeed: parseInt($refExpandedContents[3].data.match(/\d+(?!.*\d)/)) || 0,
-				physicalCores: parseInt($refExpandedContents[5].data.match(/\d+\.?\d*/)) || 0,
-				logicalCores: parseInt($refExpandedContents[5].data.match(/\d+(?!.*\d)/)) || 0,
-				rank: parseInt($refExpandedContents[7].data.match(/\d+(?!.*\d)/)) || 0,
-				samples: parseInt($refExpandedContents[9].data.match(/\d+(?!.*\d)/)) || 0,
-		 		price: $ref.find("td:nth-child(2)").text() || '',
-		 		cpuMark: parseInt($ref.find("td:nth-child(3)").text()) || 0,
-		 		cpuValue: parseInt($ref.find("td:nth-child(4)").text()) || 0,
-		 		singleThreadMark: parseInt($ref.find("td:nth-child(5)").text()) || 0,
-		 		singleThreadValue: parseInt($ref.find("td:nth-child(6)").text()) || 0,
-		 		tdp: parseInt($ref.find("td:nth-child(6)").text()) || 0,
-		 		powerPerf: parseInt($ref.find("td:nth-child(7)").text()) || 0,
-		 		testDate: (new Date($ref.find("td:nth-child(9)").text())),
-				socket: $ref.find("td:nth-child(10)").text() || '',
-				category: $ref.find("td:nth-child(11)").text() || ''
-		 	});
-		});
+			var $ = cheerio.load(html);
 
-    }else{
-    	console.log('Something went wrong');
-    }    
-});
+			$("tbody tr[id^=cpu]").each(function (x) {
+				var $ref = $(this),
+					$refExpandedContents = $ref.next().find("td div").contents();
 
-app.get('/', function(req, res){
-	res.write(JSON.stringify(cpuData)); 
+				cpuData.push({
+					name: $ref.find("td a:nth-child(2)").text() || '',
+					clockSpeed: parseInt($refExpandedContents[1].data.match(/\d+(?!.*\d)/)) || 0,
+					turboSpeed: parseInt($refExpandedContents[3].data.match(/\d+(?!.*\d)/)) || 0,
+					physicalCores: parseInt($refExpandedContents[5].data.match(/\d+\.?\d*/)) || 0,
+					logicalCores: parseInt($refExpandedContents[5].data.match(/\d+(?!.*\d)/)) || 0,
+					rank: parseInt($refExpandedContents[7].data.match(/\d+(?!.*\d)/)) || 0,
+					samples: parseInt($refExpandedContents[9].data.match(/\d+(?!.*\d)/)) || 0,
+					price: $ref.find("td:nth-child(2)").text() || '',
+					cpuMark: parseInt($ref.find("td:nth-child(3)").text()) || 0,
+					cpuValue: parseInt($ref.find("td:nth-child(4)").text()) || 0,
+					singleThreadMark: parseInt($ref.find("td:nth-child(5)").text()) || 0,
+					singleThreadValue: parseInt($ref.find("td:nth-child(6)").text()) || 0,
+					tdp: parseInt($ref.find("td:nth-child(6)").text()) || 0,
+					powerPerf: parseInt($ref.find("td:nth-child(7)").text()) || 0,
+					testDate: (new Date($ref.find("td:nth-child(9)").text())),
+					socket: $ref.find("td:nth-child(10)").text() || '',
+					category: $ref.find("td:nth-child(11)").text() || ''
+				});
+			});
+
+		} else {
+			console.log('Something went wrong');
+		}
+	});
+
+	res.write(JSON.stringify(cpuData));
 	res.end();
 });
 
